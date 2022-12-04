341232e5b443b7acedd6490b30f54db432cf67be
Awuxtron <awuxtron@gmail.com>
2022-12-04 23:33:05 +0700

    feat: turn off @typescript-eslint/no-extra-semi rule

--- typescript.js
+++ typescript.js
@@ -3,6 +3,7 @@ module.exports = {
     extends: ['plugin:@typescript-eslint/recommended'],
     plugins: ['@typescript-eslint'],
     rules: {
+        '@typescript-eslint/no-extra-semi': 'off',
         '@typescript-eslint/no-explicit-any': 'off',
         '@typescript-eslint/no-namespace': 'off',
         '@typescript-eslint/no-non-null-assertion': 'off',
